1e815a17aa63a7082188cfc90372ae363f429f9f
Igor S. Morozov <igor@morozov.is>
2019-05-25 12:31:28 +0300

    Add Arithmetics

---
+++ modules/10-basics/20-arithmetics/index.rb
@@ -0,0 +1,11 @@
+def sum_of_squares(a, b, c)
+  a**2 + b**2 + c**2
+end
+
+first_value = sum_of_squares(3, 4, 5)
+second_value = sum_of_squares(0, 2, 3)
+third_value = sum_of_squares(-5, 10, 4)
+
+puts first_value
+puts second_value
+puts third_value
--- modules/10-basics/20-arithmetics/test.rb
+++ modules/10-basics/20-arithmetics/test.rb
@@ -3,5 +3,5 @@
 require 'code_basics'
 
 CodeBasics.execute!(__dir__) do |stdout|
-  assert { stdout == '' }
+  assert { stdout == "50\n13\n141" }
 end
