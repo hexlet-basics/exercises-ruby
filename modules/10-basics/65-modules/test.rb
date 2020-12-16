c4df2617e69b3b5e689517c79e65264bdc3edfee
mokevnin <mokevnin@gmail.com>
2020-12-15 22:41:48 -0500

    update

--- modules/10-basics/65-modules/test.rb
+++ modules/10-basics/65-modules/test.rb
@@ -5,7 +5,7 @@ require './index'
 
 describe 'function' do
   it 'should works' do
-    assert { Number.reverseInt(123) == 321 }
-    assert { Number.reverseInt(-34) == -43 }
+    assert { Number.reverse_int(123) == 321 }
+    assert { Number.reverse_int(-34) == -43 }
   end
 end
