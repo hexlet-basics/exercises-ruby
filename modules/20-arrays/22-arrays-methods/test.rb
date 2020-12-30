cbac877b24f0292c3b2bf5bd2d0992f20e84ea15
mokevnin <mokevnin@gmail.com>
2020-12-29 19:03:27 -0500

    update content

--- modules/20-arrays/22-arrays-methods/test.rb
+++ modules/20-arrays/22-arrays-methods/test.rb
@@ -5,6 +5,18 @@ require './index'
 
 describe 'function' do
   it 'should works' do
-    # assert { double(3) == 6 }
+    data = [
+      ['usa', 150],
+      ['france', 37],
+      ['russia', nil]
+    ]
+    assert { amount_of_cases(data) == 187 }
+
+    data2 = [
+      ['moldova', 22],
+      ['iraq', 87],
+      ['russia', nil]
+    ]
+    assert { amount_of_cases(data2) == 119 }
   end
 end
