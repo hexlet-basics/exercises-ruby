1951d6e02258906d09efa65bfce4daa99e957bc5
Nikolay Gagarinov <feycot@gmail.com>
2021-01-17 13:51:11 +0500

    (70-summary) add more test cases

--- modules/10-basics/70-summary/test.rb
+++ modules/10-basics/70-summary/test.rb
@@ -7,5 +7,8 @@ describe 'function' do
   it 'should works' do
     assert { happy? 33 }
     assert { !happy? 3833 }
+    assert { happy? 123321 }
+    assert { happy? 666666 }
+    assert { !happy? 123456 }
   end
 end
