7a739b8b944b010cae056da9265a6931c0b6e5fd
Kirill Mokevnin <mokevnin@gmail.com>
2021-01-17 11:55:40 -0500

    improve ruby-as-a-second-language

--- modules/10-basics/15-ruby-as-a-second-language/test.rb
+++ modules/10-basics/15-ruby-as-a-second-language/test.rb
@@ -5,6 +5,7 @@ require 'test_helper'
 
 describe 'function' do
   it 'should works' do
+    puts "current time: #{Time.now}"
     date = 1.day.ago - 1.week + 3.hours
     r = Regexp.new(date.year.to_s, Regexp::MULTILINE)
     _(-> { require_relative './index' }).must_output(r)
