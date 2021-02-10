e131cdedb6742fa73658e001b169d580d8f1b104
Pavel Mikhaylov <40657512+badta5te@users.noreply.github.com>
2021-02-10 20:24:32 +0300

    fix Ruby Way solution (#95)

--- modules/10-basics/50-datatypes/index.rb
+++ modules/10-basics/50-datatypes/index.rb
@@ -1,7 +1,7 @@
 # BEGIN
 
 # Ruby Way
-# value.class in [Range, Array, hash] ? :complex : :simple
+# [Hash, Array, Range].include?(value.class) ? :complex : :simple
 
 def type_of_type(value)
   if value.is_a?(Range) || value.is_a?(Array) || value.is_a?(Hash)
