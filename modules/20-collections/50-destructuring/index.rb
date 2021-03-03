66bc14fce338d9a88f960d687fb159444d52ef33
Sergey Karpuk <sergeykarpuk@yandex.by>
2021-03-04 00:42:45 +0300

    fix(20-collections/50-destructuring): lint

--- modules/20-collections/50-destructuring/index.rb
+++ modules/20-collections/50-destructuring/index.rb
@@ -3,11 +3,11 @@
 # BEGIN
 def convert(languages)
   result = []
-  languages.each do |(name, _, typing_name)|
+  languages.each do |name, _, typing_name|
     result << [name, typing_name]
   end
   result
   # The Ruby Way
-  # languages.map{ |(name, _, typing_name)| [name, typing_name] }
+  # languages.map { |(name, _, typing_name)| [name, typing_name] }
 end
 # END
