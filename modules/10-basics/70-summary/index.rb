85bc09bd6d7f98f60dd37e91bee67c077bcaf681
Sergey Karpuk <sergeykarpuk@yandex.by>
2021-03-04 05:34:03 +0300

    fix(10-basics/70-summary): improve solution

--- modules/10-basics/70-summary/index.rb
+++ modules/10-basics/70-summary/index.rb
@@ -13,7 +13,7 @@ def happy?(number)
   num_as_str = number.to_s
   half_count = num_as_str.length / 2
   left_sum = sum_of_digits num_as_str[0, half_count]
-  right_sum = sum_of_digits num_as_str[half_count, half_count]
+  right_sum = sum_of_digits num_as_str[half_count..]
   left_sum == right_sum
 end
 # END
