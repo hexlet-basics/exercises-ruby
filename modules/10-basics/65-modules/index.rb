9c091698b16ec8f581eebe8e355f83911fa1ae62
Sergey Karpuk <sergeykarpuk@yandex.by>
2021-03-15 14:43:53 +0300

    fix(10-basics/65-modules): improve solution (#104)

--- modules/10-basics/65-modules/index.rb
+++ modules/10-basics/65-modules/index.rb
@@ -4,11 +4,7 @@
 module MyNumber
   def self.reverse_int(num)
     reversed_abs = num.abs.to_s.reverse.to_i
-    if num.positive?
-      reversed_abs
-    else
-      -reversed_abs
-    end
+    num.positive? ? reversed_abs : -reversed_abs
   end
 end
 # END
