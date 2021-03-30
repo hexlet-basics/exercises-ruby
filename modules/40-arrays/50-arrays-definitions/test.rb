9d626deed56e21434ea43855324055e10f653836
Sergey Karpuk <sergeykarpuk@yandex.by>
2021-03-30 13:42:13 +0300

    fix(40-arrays/50-arrays-definitions): impove test

--- modules/40-arrays/50-arrays-definitions/test.rb
+++ modules/40-arrays/50-arrays-definitions/test.rb
@@ -6,7 +6,7 @@ require_relative './index'
 describe 'function' do
   it 'should works' do
     assert { valid_date?('11-11-2011') }
-    assert { valid_date?('13-11-2011') }
+    assert { valid_date?('31-01-2011') }
     assert { !valid_date?('11-13-2011') }
     assert { !valid_date?('55-11-2011') }
   end
