bff6b7976c42ba7f1ac82f219f9cd460dda84069
Artem Solomatin <artem00298@gmail.com>
2022-07-12 00:08:25 +0300

    Cleanup comments

--- lib/test_helper.rb
+++ lib/test_helper.rb
@@ -4,6 +4,3 @@ require 'minitest-power_assert'
 
 require 'minitest/pride'
 require 'minitest/autorun'
-# require 'minitest/reporters'
-# Minitest::Reporters.use!
-# Minitest::Reporters.use! [Minitest::Minitap.new(color: true)]
