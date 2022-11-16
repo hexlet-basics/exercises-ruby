23beb6b3e96af7728e5e2d41976b44e5ba960172
usernaimandrey <anshlyapnikov@yandex.ru>
2022-11-16 12:47:13 +0300

    add exercise hash new

--- modules/50-hash/20-hash-new/test.rb
+++ modules/50-hash/20-hash-new/test.rb
@@ -3,7 +3,23 @@
 require 'test_helper'
 require_relative './index'
 
-# describe 'function' do
-#   it 'should works' do
-#   end
-# end
+describe 'get_adjacency_matrix' do
+  it 'should works' do
+    collection = [
+      %w[Vasy Pety],
+      %w[Vasy Oksana],
+      %w[Vasy Egor],
+      %w[Egor Pety],
+      %w[Egor Georgiy],
+      %w[Oksana Andrey]
+    ]
+
+    expected = {
+      Vasy: %w[Pety Oksana Egor],
+      Egor: %w[Pety Georgiy],
+      Oksana: ['Andrey']
+    }
+
+    assert { get_adjacency_matrix(collection) == expected }
+  end
+end
