c49800c8ec6d0411b841f8abcebc240d55ecd12a
Andrey Shlyapnikov <48034349+usernaimandrey@users.noreply.github.com>
2022-11-17 19:27:56 +0300

    add exercise hash new (#140)

--- modules/50-hash/20-hash-new/index.rb
+++ modules/50-hash/20-hash-new/index.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 # BEGIN
+def get_adjacency_matrix(collection)
+  friends = Hash.new { |h, key| h[key] = [] }
+  collection.reduce(friends) do |acc, el|
+    human, friend = el
+    acc[human.to_sym] << friend
+    acc
+  end
+  friends
+end
 # END
