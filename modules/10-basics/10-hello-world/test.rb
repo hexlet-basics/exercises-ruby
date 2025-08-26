efcb8a601917aa4bff42f72ddf440252deee4a1b
Nikolay Gagarinov <feycot@gmail.com>
2025-08-26 15:06:45 +0500

    improve test

--- modules/10-basics/10-hello-world/test.rb
+++ modules/10-basics/10-hello-world/test.rb
@@ -4,7 +4,12 @@ require 'test_helper'
 
 describe 'output' do
   it 'should works' do
-    _(-> { require_relative 'index' }).must_output(/^Hello, World!$/)
-    puts 'Hello, World!'
+    out, _err = capture_io do
+      require_relative 'index'
+    end
+
+    print out
+
+    assert { out == "Hello, World!" }
   end
 end
