508b9412536ddd2044d7b712aec09a0f261a61fa
Nikolay Gagarinov <feycot@gmail.com>
2025-08-29 16:57:52 +0500

    improve tests (add output stdout)

--- modules/10-basics/40-blocks/test.rb
+++ modules/10-basics/40-blocks/test.rb
@@ -3,18 +3,22 @@
 require 'test_helper'
 require_relative 'index'
 
-describe 'function' do
-  it 'should works 1' do
-    output = /3\n2\n1/
-    assert_output(output) do
+describe 'output' do
+  it 'should works' do
+    out, _err = capture_io do
       show_me_numbers(3)
     end
-  end
 
-  it 'should works 1' do
-    output = /4\n3\n2\n1/
-    assert_output(output) do
+    puts out
+
+    assert { out == "3\n2\n1\n" }
+
+    out2, _err = capture_io do
       show_me_numbers(4)
     end
+
+    puts out2
+
+    assert { out2 == "4\n3\n2\n1\n" }
   end
 end
